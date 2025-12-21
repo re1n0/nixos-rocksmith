1a1fe7a5f7e5207c537a44c19260b7c0ef019582
Rein <rein@rein.icu>
2025-12-21 23:17:30 +0100

    fix: version patch-rocksmith with date from last linux_rocksmith rev

--- pkgs/patch-rocksmith/default.nix
+++ pkgs/patch-rocksmith/default.nix
@@ -29,6 +29,7 @@ writeShellApplication {
     description = "Script to patch Rocksmith 2014";
     homepage = "https://github.com/theNizo/linux_rocksmith";
     license = lib.licenses.gpl3Plus;
+    version = builtins.substring 0 10 inputs.linux-rocksmith.locked.time;
     maintainers = with lib.maintainers; [
       rein
     ];
