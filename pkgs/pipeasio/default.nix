dd64a71a18cb7c205713faa8b3ca683c36458d15
Rein <rein@rein.icu>
2026-08-10 00:38:49 +0200

    fix: use rev instaed of tag in pipeasio's fetchFromGitHub

--- pkgs/pipeasio/default.nix
+++ pkgs/pipeasio/default.nix
@@ -11,23 +11,23 @@
 }:
 let
   mingw = pkgsCross.mingw32;
+  wine = wineWow64Packages.stable;
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "pipeasio";
-
   version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "M0n7y5";
     repo = "pipeasio";
-    tag = "v1.2.3";
-    hash = "sha256-aBrDXxHhbXfoUUGJ2K8I2tqRd227B/0LoTgcyQ9Ih2g=";
+    rev = "v1.5.0";
+    hash = "sha256-5CrcE27vMlRhc+Xu8TSfn1lnF4odAwaZoeh5OvcGi44=";
   };
 
   nativeBuildInputs = [
     cmake
     pkg-config
-    wineWow64Packages.stable
+    wine
     mingw.buildPackages.gcc
     mingw.buildPackages.binutils
     qt6.wrapQtAppsHook
@@ -41,13 +41,13 @@ stdenv.mkDerivation (finalAttrs: {
   cmakeFlags = [
     "-DCMAKE_BUILD_TYPE=Release"
     "-DBUILD_WOW64_32=ON"
-    "-DWINE_LIB_ROOT=${wineWow64Packages.stable}/lib/wine"
-    "-DWINE_INCLUDE_DIRS=${wineWow64Packages.stable}/include/wine;${wineWow64Packages.stable}/include/wine/windows"
+    "-DWINE_LIB_ROOT=${wine}/lib/wine"
+    "-DWINE_INCLUDE_DIRS=${wine}/include;${wine}/include/wine;${wine}/include/wine/windows"
   ];
 
   meta = {
     homepage = "https://github.com/M0n7y5/pipeasio";
-    changelog = "https://github.com/M0n7y5/pipeasio/releases/tag/${finalAttrs.src.tag}";
+    changelog = "https://github.com/M0n7y5/pipeasio/releases/tag/${finalAttrs.version}";
     description = "ASIO to pipewire driver for wine";
     license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [
