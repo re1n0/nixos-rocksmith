d3e9bed4c20de7ffb48f6e441cb97df45c427d72
Rein <rein@rein.icu>
2026-09-07 21:28:42 +0200

    fix: run ranlib on wine libraries

--- pkgs/pipeasio/default.nix
+++ pkgs/pipeasio/default.nix
@@ -9,7 +9,8 @@
   pkgsCross,
   qt6,
 }: let
-  mingw = pkgsCross.mingw32;
+  mingw64 = pkgsCross.mingwW64;
+  mingw32 = pkgsCross.mingw32;
   wine = wineWow64Packages.stable;
 in
   stdenv.mkDerivation (finalAttrs: {
@@ -23,13 +24,16 @@ in
       hash = "sha256-L7EkqebKAgVtTWFDOH3b0emFE/3Esex8T0xtbOXjNgE=";
     };
 
+    strictDeps = true;
+
     nativeBuildInputs = [
-      cmake
       pkg-config
+      cmake
       wine
-      mingw.buildPackages.gcc
-      mingw.buildPackages.binutils
+      mingw64.stdenv.cc
+      mingw32.stdenv.cc
       qt6.wrapQtAppsHook
+      qt6.qtbase.dev
     ];
 
     buildInputs = [
@@ -37,10 +41,26 @@ in
       qt6.qtbase
     ];
 
+    preConfigure = ''
+      cp -r ${wine}/lib/wine wine-lib
+      chmod -R u+w wine-lib
+
+      if [ -d wine-lib/i386-windows ]; then
+        find wine-lib/i386-windows -name '*.a' -print0 \
+          | xargs -0 -r i686-w64-mingw32-ranlib
+      fi
+
+      if [ -d wine-lib/x86_64-windows ]; then
+        find wine-lib/x86_64-windows -name '*.a' -print0 \
+          | xargs -0 -r x86_64-w64-mingw32-ranlib
+      fi
+
+      cmakeFlags+=("-DWINE_LIB_ROOT=$PWD/wine-lib")
+    '';
+
     cmakeFlags = [
       "-DCMAKE_BUILD_TYPE=Release"
       "-DBUILD_WOW64_32=ON"
-      "-DWINE_LIB_ROOT=${wine}/lib/wine"
       "-DWINE_INCLUDE_DIRS=${wine}/include;${wine}/include/wine;${wine}/include/wine/windows"
     ];
 
